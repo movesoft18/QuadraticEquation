fadb3bf0931fafdc10282730c9a143e83e46e68d
movesoft18 <movesoft@yandex.ru>
2025-02-13 09:21:21 +0400

    1

--- App.tsx
+++ App.tsx
@@ -18,30 +18,84 @@ import {
 
 
 function App(): React.JSX.Element {
+  const [koeffA, onChangeA] = useState('');
+  const [koeffB, onChangeB] = useState('');
+  const [koeffC, onChangeC] = useState('');
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.textView}>
-        <Text style = {styles.message}>Hello World!</Text>
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.dataView}>
+        <View style={styles.rowView}>
+          <Text style = {styles.message}>Квадратное уравнение</Text>
+        </View>
+        <View style={styles.rowView}>
+          <View style={styles.textView}>
+            <Text style={styles.message}>A =</Text>
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              value={koeffA}
+              onChangeText={onChangeA}
+              placeholder="Введите А"
+              keyboardType="numeric"
+            />
+          </View>
+        </View>
+        <View style={styles.rowView}>
+
+        </View>
+        <View style={styles.rowView}>
+
+        </View>
       </View>
-      
+      <View style={styles.buttonView}>
+        <TouchableOpacity>
+          <Text style = {styles.message}>Решить</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flexDirection: 'column',
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
     flex: 1,
+  },
+  dataView: {
+    flex: 9,
+    backgroundColor: 'yellow',
+  },
+  rowView: {
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '15%',
+    borderWidth: 1,
+  },
+  buttonView: {
+    flex: 1,
+    backgroundColor: 'cyan',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  inputView: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',   
+  },
+  inputField: {
+    
+  },
   message: {
-    fontSize: 28,
+    fontSize: 24,
     fontWeight: '400',
   },
 
